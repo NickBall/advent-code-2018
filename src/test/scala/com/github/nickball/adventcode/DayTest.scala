17f05c260b94b7a36d1414b716c1564084572775
Nick Ball <nick@nickball.net>
2018-12-12 11:28:25 -0800

    Refactor unit tests to allow parameterization. Implement on a couple old tests

--- src/test/scala/com/github/nickball/adventcode/DayTest.scala
+++ src/test/scala/com/github/nickball/adventcode/DayTest.scala
@@ -4,19 +4,28 @@ import org.scalatest.FunSuite
 import org.scalatest.prop.{TableDrivenPropertyChecks, TableFor2}
 
 abstract class DayTest(day: Day) extends FunSuite with TableDrivenPropertyChecks {
+
+  protected def doSolutionA: String => String = {
+    day.SolutionA
+  }
+
+  protected def doSolutionB: String => String = {
+    day.SolutionB
+  }
+
   protected def TableA(): TableFor2[String, Any]
 
   protected def TableB(): TableFor2[String, Any]
 
   forAll(TableA()) { (input: String, output: Any) =>
     test(s"'$input' applied to A should output $output") {
-      assert(day.SolutionA(input) == output.toString)
+      assert(doSolutionA(input) == output.toString)
     }
   }
 
   forAll(TableB()) { (input: String, output: Any) =>
     test(s"'$input' applied to B should output $output") {
-      assert(day.SolutionB(input) == output.toString)
+      assert(doSolutionB(input) == output.toString)
     }
   }
 }
